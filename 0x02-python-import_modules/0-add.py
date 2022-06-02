28899388752d77a0d2cd1e7c4aa0b82abdb522c8
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-06-02 18:04:44 +0000

    Fixed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import add_0
 
-a = 1
-b = 2
-print("{} + {} = {}".format(a, b, add_0.add(a, b)))
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
