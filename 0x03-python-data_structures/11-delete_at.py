640d41b389a5783fce8ba49673350872d3315610
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-06-05 10:44:52 -0700

    Delete at

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -4,9 +4,7 @@ def delete_at(my_list=[], idx=0):
         return my_list
     elif idx > (len(my_list)):
         return my_list
-    
-    for i in range(len(my_list) - 1):
-        if idx == my_list[i]:
-            del my_list[i]
+    else:
+        del my_list[idx]
 
     return my_list
