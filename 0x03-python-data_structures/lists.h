5e7979887bc0aa36c706fd294680898df68429af
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-06-06 13:01:39 -0700

    Linked list

--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -1,6 +1,7 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stdlib.h>
 /**
  * struct listint_s - singly linked list
  * @n: integer
@@ -21,4 +22,4 @@ void free_listint(listint_t *head);
 
 int is_palindrome(listint_t **head);
 
-#endif
+#endif /* LISTS_H */
