92e71ebb9714cc3d7628d7a2f62a8d2f36ad37e2
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-06-09 04:40:16 -0700

    Reduce

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from functools import reduce
+def uniq_add(my_list=[]):
+    unique_list = list(set(my_list))
+
+    sum = (reduce(lambda x, y: x + y, unique_list))
+
+    return sum
