c25808661fa07dada3708b30a6fef56eba926ef7
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-06-22 06:51:56 -0700

    try

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,14 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     num = 0
 
-    for i in range(x):
-        try:
-            print("{}".format(my_list[i]), end='')
-
-        except:
-            break
-        else:
+    try:
+        for elem in range(x):
+            print("{}".format(my_list[item]), end='')
             num += 1
-
+    except IndexError:
+        pass
     print()
     return (num)
