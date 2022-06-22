bc7037ee69fe4d212b8a81b11ae43ec61f7bde17
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-06-22 09:29:22 -0700

    Value error

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
     try:
-        if value == int:
-            return True
+        print("{:d}".format(value))
     except ValueError:
         return False
+    else:
+        return True
