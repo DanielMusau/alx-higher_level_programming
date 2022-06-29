2a854f73fe1a095d1dd8f4652de25a5eccf5c73c
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-06-29 14:21:19 -0700

    C'mon work

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -2,5 +2,13 @@
 class Rectangle:
     """ This is the creation of an empty class called
         Rectangle that defines a rectangle shape.
+
+        The Empty class has no attributes.
+
+        Args:
+            Null
+
+        Attributes:
+            Null
     """
     pass
