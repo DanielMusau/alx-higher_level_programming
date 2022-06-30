7423ce786a70b70b16e0088ef52c1c0a7d5ab153
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-06-30 02:41:36 -0700

    private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+class Square:
+    """This is a class that defines a square """
+
+    def __init__(self, size):
+        """ This is to initialize the square class and its
+            attributes.
+
+            Args:
+                size (int): size of square.
+
+            Attributes:
+                size (int): Size of the square.
+
+
+        """
+        self.size = size
