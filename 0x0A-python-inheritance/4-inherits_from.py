d74ce2c214bacdccaac2dd89fb1cc5d30dcfea67
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-07-14 09:16:37 -0700

    subclass

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,4 @@
 #!/usr/bin/python3
-"""
-Module consists of a function that checks if
-an object is an instance of a class that inherited
-from the specified class.
-"""
-
-
 def inherits_from(obj, a_class):
     """Fucntion that checks if object is an instance of a class
     that inherited from the specified class.
