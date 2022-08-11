c9273c229bf830f3e4e588e08b74484d24dd3c2c
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-08-11 03:46:02 -0700

    Changes

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
 -- Creates a new database
 -- Query Script that creates 'hbtn_0c_0' if missing
-CREATE DATABASE IF NOT EXISTS htbn_0c_0;
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- Deletes a database
 -- Query that deletes htbn_0c_0 in MYSQL SERVER
-DROP DATABASE IF EXISTS htbn_0c_0;
+DROP DATABASE IF EXISTS hbtn_0c_0;
