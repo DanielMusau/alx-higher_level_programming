16a6546d82352ef68088fc63d1e7b4e410c44b90
Danielmusau <daniel.musau@students.jkuat.ac.ke>
2022-08-11 08:42:45 -0700

    Fixed errors

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,5 +1,5 @@
 -- Script that creates databas and cities table.
-CREAATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
 	id INT UNIQUE AUTO_INCREMENT NOT NULL,
