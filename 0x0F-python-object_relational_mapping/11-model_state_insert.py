ef08d1ccf34791a0620f609e1734d91580fb5cb9
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2022-09-16 06:02:28 -0700

    Fixed errors

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -11,8 +11,8 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == '__main__':
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
                            format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           prool_pre_ping=True)
-    Session = sessionaker(bind=engine)
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind=engine)
     session = Session()
 
     newState = State(name='Louisiana')
