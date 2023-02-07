748e292a04109415ce899598a843ffc14009469f
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-02-07 18:00:39 +0300

    attributes

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,6 +5,8 @@ a new attribute to an object if it's possible.
 
 
 def add_attribute(obj_val, name, value):
+    """ Function that adds a new attribute
+    to an object. """
     if not hasattr(obj_val, "__dict__"):
         raise TypeError("can't add new attribute")
 
