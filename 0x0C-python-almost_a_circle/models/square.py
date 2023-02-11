b81610eaf3dfefe6278e883a4c686f5d1e273636
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-02-11 18:24:26 +0300

    Dictionary

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -35,3 +35,7 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ Returns the dictionary representation of a Square. """
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
