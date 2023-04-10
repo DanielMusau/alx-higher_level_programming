5c5113cc26d2dcd3d21aa0bcf2360e0976954501
DanielMbithiMusau <94052405+DanielMbithiMusau@users.noreply.github.com>
2023-04-10 12:30:59 +0300

    Delete 0x12-javascript-warm_up directory

--- 0x12-javascript-warm_up/4-concat.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-
-const cmdArgs = process.argv;
-
-console.log(cmdArgs[2] + ' is ' + cmdArgs[3]);
--- 0x12-javascript-warm_up/5-to_integer.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const cmdArgs = parseInt(process.argv[2]);
-
-if (Number.isNaN(cmdArgs)) {
-  console.log('Not a number');
-} else {
-  console.log('My number: ' + cmdArgs);
-}
