1397bec7c560ff2f6a6341eee4e12fcb7d49cfff
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-04-16 13:00:33 +0300

    Filter

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -8,18 +8,15 @@ if __name__ == '__main__':
     password = sys.argv[2]
     database = sys.argv[3]
 
-db = MySQLdb.connect(host="localhost",
-                     port=3306,
-                     user=username,
-                     passwd=password,
-                     db=database
-                     )
-cur = db.cursor()
-cur.execute("SELECT * FROM states ORDER BY id ASC;")
-rows = cur.fetchall()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database
+                         )
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC;")
+    rows = cur.fetchall()
 
-for row in rows:
-    print(row)
-
-cur.close()
-db.close()
+    for row in rows:
+        print(row)
