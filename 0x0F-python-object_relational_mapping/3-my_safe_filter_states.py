2ea05944e7f573f02b03ec64dc4386418d5db438
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-04-16 13:54:25 +0300

    Safe filter

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -7,9 +7,9 @@ import sys
 
 if __name__ == '__main__':
     username = sys.argv[1]
-    password =  sys.argv[2]
-    database =  sys.argv[3]
-    state_name =  sys.argv[4]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
 
     db = MySQLdb.connect(host='localhost',
                          port=3306,
