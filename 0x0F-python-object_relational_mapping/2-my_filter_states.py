0b986f3822b0ba80fcb5246c5431ebdace537e54
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-04-16 13:19:07 +0300

    Checking

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     cur.execute(f"SELECT * FROM states \
                  WHERE CONVERT(`name` USING Latin1) \
                  COLLATE Latin1_General_CS = '{state_name}';"
-                 )
+                )
 
     rows = cur.fetchall()
 
