d931ba559fb391193dd3a4ca85b784aaaec2ea55
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-04-16 13:20:16 +0300

    Checking

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""Script that takes in an argument and displays all
-values in the states table."""
-import MySQLdb
+"""
+Lists all values in the states tables of a database where name
+matches the argument
+"""
 import sys
+import MySQLdb
 
 if __name__ == '__main__':
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-    state_name = sys.argv[4]
-
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=username,
-                         passwd=password,
-                         db=database
-                         )
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3], port=3306)
 
     cur = db.cursor()
-    cur.execute(f"SELECT * FROM states \
-                 WHERE CONVERT(`name` USING Latin1) \
-                 COLLATE Latin1_General_CS = '{state_name}';"
-                )
+    cur.execute("SELECT * \
+    FROM states \
+    WHERE CONVERT(`name` USING Latin1) \
+    COLLATE Latin1_General_CS = '{}';".format(sys.argv[4]))
+    states = cur.fetchall()
 
-    rows = cur.fetchall()
-
-    for row in rows:
-        print(row)
+    for state in states:
+        print(state)
