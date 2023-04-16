5d17de693b335b1e66a75b8bcd6a1ab78e202243
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-04-16 13:03:15 +0300

    Filter

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,16 +9,20 @@ if __name__ == '__main__':
     password = sys.argv[2]
     database = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost',
-                     port=3306,
-                     user=username,
-                     passwd=password,
-                     db=database
-                     )
+    db = MySQLdb.connect(host='localhost',
+                         port=3306,
+                         user=username,
+                         passwd=password,
+                         db=database
+                         )
 
-cur = db.cursor()
-cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
-rows = cur.fetchall()
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-for row in rows:
-    print(row)
+    rows = cur.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
