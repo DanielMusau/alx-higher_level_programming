dfdf640dadb46fd196efa3491d7152aa1b0aa0e2
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-04-27 14:12:18 +0300

    Finding peak among integers

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,11 +5,10 @@ a list of unsorted integers.
 
 
 def find_peak(list_of_integers):
-        """Function that finds a peak in a
-        list of unsorted integers."""
-        if len(list_of_integers) == 0:
-                return None
+    """Function that finds a peak in a
+    list of unsorted integers."""
+    if len(list_of_integers) == 0:
+        return None
 
-        list_of_integers.sort()
-        
-        return list_of_integers[-1]
+    list_of_integers.sort()
+    return list_of_integers[-1]
