2da273dbbba66f3d7f002ecee3a1f9293f0117f8
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-04-27 14:53:47 +0300

    Fixing error

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -29,7 +29,7 @@ def find_peak(list_of_integers):
 
         if list_of_integers[mid] >= list_of_integers[mid - 1] and \
                 list_of_integers[mid] >= list_of_integers[mid + 1]:
-                    return list_of_integers[mid]
+            return list_of_integers[mid]
 
         elif list_of_integers[mid - 1] > list_of_integers[mid]:
             right = mid - 1
